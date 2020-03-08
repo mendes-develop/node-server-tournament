25d230eed30b367c70a50e6c71bc3ec4cc29a404
Alex Mendes <mendes.develop@gmail.com>
2020-03-08 15:05:29 -0400

    Connecting to mongo db

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
-
-
-const PORT = process.env.PORT || 3001
+const mongoose = require("mongoose");
 const app = express();
+require('dotenv/config')
 
-//Routes
+const PORT = process.env.PORT || 3001;
 
+
+//Routes are going to come here
+app.get("/", (req, resp) => {
+    resp.send("<h1>We are running the ' / ' page.</h1>");
+});
+
+//Connect to MongoDB
+mongoose.connect(
+  process.env.ATLAS_URI,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => {
+    console.log("Connected to MongoBD");
+  }
+);
 
 // App listening to the server
-app.listen((PORT), () => console.log("Running on port 3001"))
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
